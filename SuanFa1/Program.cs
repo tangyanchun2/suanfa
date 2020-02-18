3f3c1d5149bbe2621ad557149749887d6f6c7db7
tangyanchun2 <tangyanchun2@hotmail.com>
2020-02-18 13:37:37 +0800

    添加项目文件。

---
+++ SuanFa1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SuanFa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.ReadKey();
+        }
+
+        public static void GetContinueNumber()
+        {
+
+        }
+    }
+}
